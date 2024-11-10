091cbc8a44efcea1b6e4a1b016189cea5fb69496
vvg127 <582d97@naver.com>
2024-11-10 21:57:08 +0900

    WildStock

--- src/main/java/org/stocks/wildStock/Commands/Shop.java
+++ src/main/java/org/stocks/wildStock/Commands/Shop.java
@@ -5,6 +5,7 @@ import net.kyori.adventure.text.format.Style;
 import net.kyori.adventure.text.format.TextColor;
 import net.kyori.adventure.text.format.TextDecoration;
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -14,7 +15,12 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.jetbrains.annotations.NotNull;
+import org.stocks.wildStock.Library.Data;
+
+import java.util.ArrayList;
 
 public class Shop implements CommandExecutor, Listener {
     /*TODO 상점
@@ -30,6 +36,20 @@ public class Shop implements CommandExecutor, Listener {
             Inventory inventory = Bukkit.createInventory(p, 54, Component.text("상점",
                     Style.style(TextColor.color(0, 0, 0), TextDecoration.ITALIC.withState(false))));
             p.openInventory(inventory);
+
+            ItemStack money = new ItemStack(Material.SUNFLOWER);
+            ItemMeta meta = money.getItemMeta();
+            ArrayList<Component> list = new ArrayList<>();
+
+            list.add(Component.text(Data.getMoney(p.getUniqueId()) + " 원", Style.style(TextColor.color(255, 191, 0), TextDecoration.ITALIC.withState(false))));
+            meta.lore(list);
+            list.clear();
+            meta.displayName(Component.text("보유 자금", Style.style(TextColor.color(0, 0, 0), TextDecoration.ITALIC.withState(false))));
+            money.setItemMeta(meta);
+            inventory.setItem(5, money);
+
+            //TODO meta랑 list는 재활용
+
 
             p.playSound(p, Sound.BLOCK_NOTE_BLOCK_BELL, 1, 1);
         }
